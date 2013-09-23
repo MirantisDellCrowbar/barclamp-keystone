7b8d78de64d5ab84c09eb3bbe907fadf88873c7e
Evgeniy Shurmin <eshurmin@mirantis.com>
2013-09-23 14:39:06 +0400

    fix wsgi bin and typo

--- chef/cookbooks/keystone/recipes/server.rb
+++ chef/cookbooks/keystone/recipes/server.rb
@@ -244,7 +244,7 @@ template "/etc/keystone/keystone.conf" do
 end
 
 execute "keystone-manage db_sync" do
-  command "#{venv_prefix}keystone-manage db_sync"
+  command "keystone-manage db_sync"
   action :run
 end
 
